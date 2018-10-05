c77d6533ba25f8a03bb66077bb663d0e1b28cd0d
João Victor <jv-10-victor@live.com>
2018-10-05 12:58:10 -0300

    Aula 1

--- src/pages/produto-detalhe/produto-detalhe.ts
+++ src/pages/produto-detalhe/produto-detalhe.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the CategoriasPage page.
+ * Generated class for the ProdutoDetalhePage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,18 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-categorias',
-  templateUrl: 'categorias.html',
+  selector: 'page-produto-detalhe',
+  templateUrl: 'produto-detalhe.html',
 })
-export class CategoriasPage {
+export class ProdutoDetalhePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CategoriasPage');
+    console.log('ionViewDidLoad ProdutoDetalhePage');
   }
-navProdutos(){
-  this.navCtrl.push('ProdutosPage');
-}
+
 }
